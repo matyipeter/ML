c53a0c0ffbcdd9f947c8ea816b4842507714bf38
peter <petermatyi2004@gmail.com>
2023-09-28 21:16:25 +0200

    Added max temperature func to weather.py

--- api/weather.py
+++ api/weather.py
@@ -2,7 +2,7 @@ import requests
 from pprint import pprint
 
 API_KEY = "9d39043234930007b2bd308ab2da17b8"
-API_URL = "https://api.openweathermap.org/data/2.5/weather"
+API_URL = "https://api.openweathermap.org/data/2.5/forecast"
 GEO_API_URL = "http://api.openweathermap.org/geo/1.0/zip"
 
 def get_coords(zip, cc):
@@ -17,9 +17,28 @@ def get_weather(lat, lon):
     data = weather.json()
     return data
 
+def day_temp(data):
+    days = data["list"]
+    day_temp = {}
+    for i in days:
+        if i['dt_txt'][8:10] in day_temp:
+            day_temp[i['dt_txt'][8:10]].append(i['main']['temp'])
+        else:
+            day_temp[i['dt_txt'][8:10]] = []
+            day_temp[i['dt_txt'][8:10]].append(i['main']['temp'])
+    
+    day_max = {}
+    for i in day_temp:
+        day_max[i] = max(day_temp[i])
+
+    return day_max
+
+
 def main():
     lat, lon = get_coords(2094, "HU")
     weather = get_weather(lat, lon)
-    pprint(weather)
+    temps = day_temp(weather)
+    for i in temps:
+        print(f'{i}. {temps[i]} fok lesz a maximum')
 
 main()
