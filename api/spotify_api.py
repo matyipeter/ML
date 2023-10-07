d25ec3753950a6e4b4e03d4ef3c3ef3431227616
peter <petermatyi2004@gmail.com>
2023-10-07 20:07:06 +0200

    Spotify API setup

--- api/spotify_api.py
+++ api/spotify_api.py
@@ -1,17 +1,73 @@
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 import json
+import random
 
+# scope = "user-library-read"
 
-client_id = "73414a0f72294f98a0de7edc95f87376"
-client_secret = "e31085cdb8b247f2a62607d53041f088"
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-music = sp.search(q="Let%20her%20go", type='track', limit=10)
+# music = sp.search(q="Let%20her%20go", type='track', limit=10)
 
-with open('files/lethergo.json', 'w') as output:
-    json.dump(music, output, indent=3)
+# with open('files/lethergo.json', 'w') as output:
+#     json.dump(music, output, indent=3)
     
+# print(music['tracks']['items'][0])
+
+
+# preparations
+scope = "user-library-read"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+# user input
+artist = input('Who is your favourite artist? ')
+
+# get the name of the albums
+def get_albums(artist):
+    albums = sp.search(q=f'artist:{artist}',type='album', limit=30)
+    albums = albums['albums']['items']
+    album_names = [i['name'] for i in albums]
+    return album_names
+
+albums = get_albums(artist)
+choice = random.choice(albums).split()
+
+#print(choice)
+max_len = choice[0]
+for i in choice:
+    if len(i) > len(max_len):
+        max_len = i
+
+def create_guess_word(max_len):
+    guess_word = []
+    for _ in range(len(max_len)):
+        guess_word.append('_')
+    return guess_word
+
+def guess_assess(guess_word, guess, max_len):
+    if guess == max_len:
+        print('Kitaláltad!')
+        return True
+    else:
+        for index,item in enumerate(max_len):
+            try:
+                if item == guess[index]:
+                    guess_word[index] = item
+            except:
+                print('he')
+        print("".join(guess_word))
+    
+guess_word = create_guess_word(max_len)
+while True:
+    guess = input('Take a guess: ')
+    if guess_assess(guess_word, guess, max_len):
+        break
+    
+
+
+
+
+
 
 
 
